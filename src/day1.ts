407819030ef8c5dbc7fced4553602aa5fd67b9d9
makaronma <87429019+makaronma@users.noreply.github.com>
2022-12-01 23:44:25 +0800

    add timeDuration measure

--- src/day1.ts
+++ src/day1.ts
@@ -4,17 +4,24 @@ import { chopBy, getTopsOfNums } from "./utils";
 // https://adventofcode.com/2022/day/1
 
 export default () =>
-  fs.readFile("day1.txt", "utf8", (err, data) => {
-    if (err) throw err;
+  {
+    const startTimeInMs = new Date().getTime();
+    fs.readFile("day1.txt", "utf8", (err, data) => {
+      if (err) throw err;
 
-    const calories = data.toString().split("\n");
+      const calories = data.toString().split("\n");
 
-    const snackCalories = chopBy(calories, "");
+      const snackCalories = chopBy(calories, "");
 
-    const maxs = snackCalories.map((cs) => cs.reduce((a, b) => +a + +b, 0));
-    const highestOne = Math.max(...maxs);
-    console.log({ highestOne });
+      const maxs = snackCalories.map((cs) => cs.reduce((a, b) => +a + +b, 0));
+      const highestOne = Math.max(...maxs);
+      console.log({ highestOne });
 
-    const top3 = getTopsOfNums(maxs, 3).reduce((a, b) => a + b, 0);
-    console.log({ top3 });
-  });
+      const top3 = getTopsOfNums(maxs, 3).reduce((a, b) => a + b, 0);
+      console.log({ top3 });
+
+      const endTimeInMs = new Date().getTime();
+      const durationInMs = endTimeInMs - startTimeInMs;
+      console.log({ durationInMs });
+    });
+  }

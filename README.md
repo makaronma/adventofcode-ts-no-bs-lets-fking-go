8c80ac8883b1fdd95947dd8194c97b02114b0321
makaronma <87429019+makaronma@users.noreply.github.com>
2022-12-01 22:32:03 +0800

    chore: environment setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# adventofcode-ts-no-bs-lets-fking-go
+
+```shell
+pnpm i
+```
+
+```shell
+pnpm dev
+```
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { camelCase } from "lodash";
+
+
+console.log(camelCase('asd asd qwd -qd _D-s asd '));
